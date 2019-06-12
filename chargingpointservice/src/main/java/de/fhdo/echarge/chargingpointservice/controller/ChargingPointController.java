8817d657f204486a584e4fcdbc55647d8562fd7d
ADESSO\kahriman <Goekhan.Kahriman@adesso-insurance-solutions.de>
2019-06-12 13:25:36 +0200

    Environment Info Controller und Frontend für Environment Info Service erstellt

--- chargingpointservice/src/main/java/de/fhdo/echarge/chargingpointservice/controller/ChargingPointController.java
+++ chargingpointservice/src/main/java/de/fhdo/echarge/chargingpointservice/controller/ChargingPointController.java
@@ -1,7 +1,5 @@
 package de.fhdo.echarge.chargingpointservice.controller;
 
-import java.util.List;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +9,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import de.fhdo.echarge.chargingpointservice.entity.ChargingPoint;
