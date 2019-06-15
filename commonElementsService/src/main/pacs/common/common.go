67a32eb8189b2a025f59409bc5964a8b8a4bd98d
ADESSO\kahriman <Goekhan.Kahriman@adesso-insurance-solutions.de>
2019-06-15 16:07:58 +0200

    Service für gemeinsame HTML-Elemente definiert (vgl. https://github.com/ewolff/SCS-ESI/tree/master/scs-demo-esi-common), projektspezifische Anpassungen vorgenommen

---
+++ commonElementsService/src/main/pacs/common/common.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.Handle("/common/css/", http.StripPrefix("/common/css/", http.FileServer(http.Dir("/css"))))
+	http.HandleFunc("/common/header-1.0", Header)
+	http.HandleFunc("/common/titlepacs-1.0", TitlePacs)
+	http.HandleFunc("/common/navbar-1.0", Navbar)
+	http.HandleFunc("/common/footer-1.0", Footer)
+	fmt.Println("Starting up on 8180")
+	log.Fatal(http.ListenAndServe(":8180", nil))
+}
+
+func Header(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(w, `
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+	  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+	 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+        `)
+}
+
+func TitlePacs(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(w, `
+			<div class="jumbotron text-center">
+				<h2>Park and Charge System  - User Management</h2>
+				<h3>Included with ESI</h3>
+			</div>`)
+}
+
+func Navbar(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(w, `
+			<nav class="navbar navbar-default">
+			  <div class="container-fluid">
+				<div class="navbar-header">
+				  <a class="navbar-brand" href="#">PACS - ESI</a>
+				</div>
+				<ul class="nav navbar-nav">
+				  <li class="active"><a href="http://localhost:8080/">UserManagement</a></li>
+				  <li><a href="http://localhost:8081/" target="_blank">ChargingPoint</a></li>
+				  <li><a href="http://localhost:8082/" target="_blank">Environment</a></li>
+				</ul>
+			  </div>
+			</nav>`)
+}
+
+func Footer(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(w, `
+				<p>© 2019 Copyright: Gökhan Kahriman - ESI</p>`)
+}
