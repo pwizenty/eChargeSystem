d15369183f818718139c43daa4a6ec7054af9d18
ADESSO\kahriman <Goekhan.Kahriman@adesso-insurance-solutions.de>
2019-07-24 08:06:49 +0200

    Common-Service und index.html überarbeitet

--- commonElementsService/src/main/pacs/common/common.go
+++ commonElementsService/src/main/pacs/common/common.go
@@ -18,9 +18,12 @@ func main() {
 
 func Header(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintln(w, `
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 	  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 	 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+		<title>PACS - User</title>
         `)
 }
 
